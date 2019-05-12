13ca0792e71de81d2d614e3812dd114944eda4ff
AndresMarconi <49106769+AndresMarconi@users.noreply.github.com>
2019-05-12 12:22:03 -0300

    Add files via upload

---
+++ login.php
@@ -0,0 +1,40 @@
+<?php
+	session_start();
+	include('objetos.php');
+	$home = new HomeSwitch();
+
+	echo '<!DOCTYPE html>'."\n";
+	echo '<html>'."\n";
+		include('head.html');
+		echo '<body>'."\n";
+			include('login.html');
+			include('footer.html');
+		echo "\n".'</body>'."\n";
+	echo '</html>';
+
+	// pregunta de logeo
+		if (isset($_POST['logeo'])){
+		 	if ($_POST['nombre_usuario'] != "" and $_POST['password'] != ""){
+		 		if ($_POST['tipo'] == 'usuario') {
+		 			$usu = $home->obtenerUsu($_POST['nombre_usuario']);
+		 		} else {
+		 			$usu = $home->obtenerAdmin($_POST['nombre_usuario']);
+		 		}
+				if($usu->acceso($_POST['password'])){
+
+					$_SESSION['usu']=$_POST["nombre_usuario"];
+					$usu->login();
+				} else {
+					echo "<script languaje= 'javascript'>";
+					echo "window.location='login.php';";
+					echo "alert ('Datos de usuario incorrectos')";
+					echo "</script>";
+				}
+			} else {
+				echo "<script languaje= 'javascript'>";
+				echo "window.location='login.php';";
+				echo "alert ('Complete todos los campos')";
+				echo "</script>";
+			}
+		}
+?>
---
+++ objetos.php
@@ -0,0 +1,22 @@
+<?php
+	date_default_timezone_set('America/Argentina/Buenos_Aires');
+
+	require_once 'Model/homeswitch.entidad.php';
+	require_once 'Model/usuario.entidad.php';
+	require_once 'Model/usuario.model.php';
+	require_once 'Model/admin.entidad.php';
+	require_once 'Model/admin.model.php';
+	require_once 'Model/residencia.entidad.php';
+	require_once 'Model/residencia.model.php';
+	//require_once 'Model/basico.model.php';
+	require_once 'Model/basico.entidad.php';
+	require_once 'Model/premiun.entidad.php';
+	//require_once 'Model/premiun.model.php';
+	require_once 'Model/reserva.entidad.php';
+	require_once 'Model/directa.entidad.php';
+	//require_once 'Model/directa.model.php';
+	require_once 'Model/subasta.entidad.php';
+	//require_once 'Model/subasta.model.php';
+	require_once 'Model/hotsale.entidad.php';
+	//require_once 'Model/hotsale.model.php';
+?>
