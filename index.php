283d3355cb87fd0241033320b8e64b9a8c2320dc
AndresMarconi <49106769+AndresMarconi@users.noreply.github.com>
2019-05-12 12:05:40 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+	echo '<!DOCTYPE html>'."\n";
+	echo '<html>'."\n";
+		include('head.html');
+		echo '<body>'."\n";
+			include('navbar.html');
+			include('index.html');
+			include('footer.html');
+		echo "\n".'</body>'."\n";
+	echo '</html>';
+?>
